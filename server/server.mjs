f583898d97cd67885370e37ce440bf122212b4cb
Victor Zamanian <victor.zamanian@gmail.com>
2022-09-12 23:57:53 +0200

    Add more relay routes.

--- server/server.mjs
+++ server/server.mjs
@@ -1,22 +1,43 @@
 import express from "express";
 
+async function relay(url, res) {
+  const r = await fetch(url);
+  const { data: json } = await r.json();
+  res.set("Access-Control-Allow-Origin", "http://localhost:5173");
+  res.json(json);
+  console.log(`${JSON.stringify(json).substring(0, 24)}...`);
+}
+
 const app = express();
 const port = 3000;
 
-app.get("/:id", async (req, res) => {
-  console.log("requesting data...");
-  const trackerResponse = await fetch(
-    `https://api.tracker.gg/api/v2/rocket-league/standard/profile/steam/${req.params.id}`
+app.get("/profile/:platformId", (req, res) => {
+  const { platformId } = req.params;
+  relay(
+    `https://api.tracker.gg/api/v2/rocket-league/standard/profile/steam/${platformId}`,
+    res
   );
+});
 
-  const data = await trackerResponse.json();
+app.get("/mmr/:playerId", (req, res) => {
+  const { playerId } = req.params;
+  relay(
+    `https://api.tracker.gg/api/v1/rocket-league/player-history/mmr/${playerId}`,
+    res
+  );
+});
 
-  res.set("Access-Control-Allow-Origin", "http://localhost:5173");
-  res.json(data);
-
-  console.log(`${JSON.stringify(data).substring(0, 24)}...`);
+app.get("/distribution/:playlistId", (req, res) => {
+  const { playlistId } = req.params;
+  const { playlist } = req.query;
+  relay(
+    `https://api.tracker.gg/api/v1/rocket-league/distribution/${playlistId}${
+      playlist ? `?playlist=${playlist}` : ""
+    }`,
+    res
+  );
 });
 
 app.listen(port, () => {
-  console.log(`Tracker.gg relay listening on port ${port}`);
+  console.log(`tracker.gg relay listening on port ${port}`);
 });
