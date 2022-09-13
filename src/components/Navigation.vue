7079a9e40b1f0dd23f9f25ec57040ef92ba6ec44
Victor Zamanian <victor.zamanian@gmail.com>
2022-09-13 15:52:41 +0200

    Add navigation component skeleton.

---
+++ src/components/Navigation.vue
@@ -0,0 +1,49 @@
+<script lang="ts" setup>
+import { ref, watch } from "vue";
+
+const activeTab = ref("overview");
+
+watch(
+  () => activeTab.value,
+  () => console.log(activeTab.value)
+);
+</script>
+
+<template>
+  <nav>
+    <input
+      v-model="activeTab"
+      id="overview"
+      name="tab"
+      type="radio"
+      value="overview"
+    />
+    <label for="overview">Overview</label>
+    <input
+      v-model="activeTab"
+      id="history"
+      name="tab"
+      type="radio"
+      value="history"
+    />
+    <label for="history">History</label>
+  </nav>
+</template>
+
+<style scoped>
+nav {
+  display: flex;
+}
+input[type="radio"] {
+  display: none;
+}
+
+label {
+  user-select: none;
+  cursor: pointer;
+}
+
+input:checked + label {
+  color: red;
+}
+</style>
