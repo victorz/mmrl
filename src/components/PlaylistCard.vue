cf20defc38077dd63a61a0993d476da9b41ccaa1
Victor Zamanian <victor.zamanian@gmail.com>
2022-09-16 22:43:05 +0200

    Set percentage color.
    Fades from red (low rank) to green (high rank).

--- src/components/PlaylistCard.vue
+++ src/components/PlaylistCard.vue
@@ -9,6 +9,12 @@ const percentile = computed(() =>
     ? `Bottom ${playlist.rating.percentile} %`
     : `Top ${(100 - playlist.rating.percentile).toFixed(1)} %`
 );
+const percentileColor = computed(
+  () =>
+    `rgb(${(255 * (100 - playlist.rating.percentile)) / 100}, ${
+      (255 * playlist.rating.percentile) / 100
+    }, 0)`
+);
 </script>
 
 <template>
@@ -16,7 +22,10 @@ const percentile = computed(() =>
     <img :src="playlist.tier.iconUrl" :alt="playlist.tier.name" />
     <span>
       <div>{{ playlist.playlistName }}</div>
-      <div>{{ playlist.division.name }} ({{ percentile }})</div>
+      <div>
+        {{ playlist.division.name }}
+        <span class="percentile">({{ percentile }})</span>
+      </div>
     </span>
   </div>
 </template>
@@ -28,6 +37,17 @@ const percentile = computed(() =>
   grid-template-rows: 1fr;
   align-items: center;
   width: 100%;
+  white-space: nowrap;
+}
+.percentile {
+  font-size: smaller;
+  opacity: 80%;
+  color: v-bind(percentileColor);
+}
+@media (prefers-color-scheme: light) {
+  .percentile {
+    filter: brightness(50%);
+  }
 }
 img {
   width: 56px;
